bdba267768b9ed4c8553abac5aa40b1c79ef2720
Gail Macleod <160984718+GailMacleod@users.noreply.github.com>
2025-07-28 16:44:01 +1000

    Update schema.ts

--- shared/schema.ts
+++ shared/schema.ts
@@ -9,4 +9,28 @@ import * as user from './userSchema';
 import * as analytics from './analyticsSchema';
 import * as platform from './platformSchema';
 
-export { ...auth, ...post, ...user, ...analytics, ...platform };
+export {
+// List all exports explicitly
+sessions from './authSchema',
+enhancedOauthTokens from './authSchema',
+oauthTokens from './authSchema',
+postSchedule from './postSchema',
+postLedger from './postSchema',
+posts from './postSchema',
+postLogs from './postSchema',
+enhancedPostLogs from './postSchema',
+users from './userSchema',
+brandPurpose from './userSchema',
+verificationCodes from './userSchema',
+giftCertificates from './userSchema',
+giftCertificateActionLog from './userSchema',
+subscriptionAnalytics from './analyticsSchema',
+videoUsage from './analyticsSchema',
+quotaUsage from './analyticsSchema',
+notificationLogs from './analyticsSchema',
+platformConnections from './platformSchema',
+// Add any insert schemas similarly
+insertUserSchema from './userSchema',
+insertPostSchema from './postSchema',
+// etc. for all insert schemas
+};
